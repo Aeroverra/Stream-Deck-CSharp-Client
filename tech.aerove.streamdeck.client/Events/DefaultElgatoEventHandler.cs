18010057fdcba0e6d241826e7cf0c12efd0fbb44
Aeroverra <88tontos@gmail.com>
2022-04-16 20:09:57 -0500

    Events wait for globalsettings

--- tech.aerove.streamdeck.client/Events/DefaultElgatoEventHandler.cs
+++ tech.aerove.streamdeck.client/Events/DefaultElgatoEventHandler.cs
@@ -16,18 +16,29 @@ namespace tech.aerove.streamdeck.client.Events
         private readonly IActionFactory _factory;
         private readonly IActionExecuter _executer;
         private readonly ICache _cache;
+        private readonly IElgatoDispatcher _dispatcher;
+        private bool FirstEventHandled = false;
+        private bool AwaitingInitialGlobalSettings = true;
+        private List<string> PendingMessages = new List<string>();
         public DefaultElgatoEventHandler(ILogger<DefaultElgatoEventHandler> logger,
             IActionFactory factory, IActionExecuter executer,
-            ICache cache)
+            ICache cache, IElgatoDispatcher dispatcher)
         {
             _logger = logger;
             _factory = factory;
             _executer = executer;
             _cache = cache;
+            _dispatcher = dispatcher;
         }
 
         public async Task HandleIncoming(string message)
         {
+            if (!FirstEventHandled)
+            {
+                FirstEventHandled = true;
+                await _dispatcher.GetGlobalSettingsAsync();
+            }
+
             _logger.LogDebug("{resultString}", message);
             var elgatoEvent = ElgatoEvent.FromJson(message);
             if (elgatoEvent == null)
@@ -35,11 +46,26 @@ namespace tech.aerove.streamdeck.client.Events
                 _logger.LogWarning("Could not parse event: {resultString}", message);
                 return;
             }
+            if (AwaitingInitialGlobalSettings && elgatoEvent.Event != ElgatoEventType.DidReceiveGlobalSettings)
+            {
+                PendingMessages.Add(message);
+                return;
+            }
+
             _logger.LogDebug("Received: {eventtype}", elgatoEvent.Event);
 
             _cache.Update(elgatoEvent);
             var actions = _factory.CreateActions(elgatoEvent);
             await _executer.ExecuteAsync(elgatoEvent, actions);
+
+            if (AwaitingInitialGlobalSettings)
+            {
+                AwaitingInitialGlobalSettings = false;
+                foreach(var pendingmessage in PendingMessages)
+                {
+                    await HandleIncoming(pendingmessage);
+                }
+            }
         }
 
 
